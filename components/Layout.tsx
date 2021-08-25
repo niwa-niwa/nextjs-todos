93129c33b89cf12ed87393786e31c6a7f089487c
niwa-niwa <urban.hang274@gmail.com>
2021-08-25 14:48:12 +0900

    implemented Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+
+export default function Layout({
+  children,
+  title = "Default title",
+}: {
+  children: ReactNode;
+  title: string;
+}) {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex flex-1 justify-center items-center w-screen flex-col">
+        {children}
+      </main>
+      <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
+        @Next.js with TypeScript
+      </footer>
+    </div>
+  );
+}
